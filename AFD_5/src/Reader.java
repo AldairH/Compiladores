333421919bc53f97348665fdedc9b4f2aa294d2e
AldairH <aldairhj@outlook.com>
2025-03-18 00:38:44 -0600

    AFD_5

--- AFD_5/src/Reader.java
+++ AFD_5/src/Reader.java
@@ -6,14 +6,25 @@ class Reader {
             String linea;
             int numLineas = 1;
             while ((linea = br.readLine()) != null){
+                int indice = linea.indexOf(";");
+                if (indice == -1) {
+                    System.out.println("Línea " + numLineas + " no contiene ';'.");
+                    numLineas++;
+                    continue;
+                }
+                linea = linea.substring(0, indice).trim();
+                if (linea.isEmpty()) {
+                    numLineas++;
+                    continue;
+                }
                 String[] palabras = linea.split(" ");
                 int numPalabra = 1;
                 for (int i = 0; i < palabras.length; i++){
                     boolean resultado = afd.afd(palabras[i]);
                     if (!resultado) {
-                        System.out.println("Error en la linea " + numLineas + " palabra " + numPalabra + ".");
+                        System.out.println("Error en la linea " + numLineas + " palabra " + numPalabra + ":" + palabras[i]);
                     }else{
-                        System.out.println("Palabra " + numPalabra + " de linea " + numLineas + " aceptada.");
+                        System.out.println("Palabra " + numPalabra + " de linea " + numLineas + " aceptada: " + palabras[i]);
                     }
                     numPalabra++;
                 }
