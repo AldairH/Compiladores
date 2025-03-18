7eef09077ddd67c0208a822a0b7b1064dba4d8ca
AldairH <aldairhj@outlook.com>
2025-03-18 00:06:36 -0600

    Prueba

--- AFD_finDeCadena/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        reader lector = new reader();
-        String nombreArchivo = "entrada.txt";
-        lector.read(nombreArchivo);
-    }
-}
